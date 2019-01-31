263b41b4cc7136191734b60ec104e56436edf2c9
Mathieu Degand <mat.degand@numericable.fr>
2019-01-31 10:46:13 +0100

    ajout socket

--- webserver/main.c
+++ webserver/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <string.h>
-
-int main(int argc, char **argv) {
+#include "socket.h"
+int main(void) {
     /* Arnold Robbins in yhe LJ of February '95, describing RCS */
-    if (argc > 1 && strcmp(argv[1], "-advice") == 0) {
+    /*if (argc > 1 && strcmp(argv[1], "-advice") == 0) {
         printf("Don't Panic!\n");
         return 42;
     }
     printf("Need an advice?\n");
+    return 0;*/
+
+    while(1) {
+        creer_serveur(8080);      
+    }
+
     return 0;
+    
 }
---
+++ webserver/socket.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "socket.h"
+#include <sys/types.h>         
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+
+int creer_serveur(int port) {
+
+// On crée d'abord la socket
+int socket_serveur ;
+socket_serveur = socket (AF_INET, SOCK_STREAM , 0);
+
+	if (socket_serveur == -1) {
+	/* traitement de l ’ erreur */
+	perror("socket_serveur");
+	}
+	/* Utilisation de la socket serveur */
+
+	// Ensuite on configure et attache la socket à une interface réseau 
+	struct sockaddr_in saddr;
+	saddr.sin_family = AF_INET; /* Socket ipv4 */
+	saddr.sin_port = htons(port); /* Port d’écoute */
+	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
+
+	if (bind (socket_serveur,(struct sockaddr *) &saddr, sizeof(saddr)) == -1) {
+		/* traitement de l ’ erreur */
+		perror("bind socker_serveur");
+	}
+	
+	// Enfin on demande au système de pouvoir écouter sur le port et de pouvoir attendre des connexions 
+	if (listen (socket_serveur, 10) == -1) {
+		perror ("listen socket_serveur");
+		/* traitement d ’ erreur */
+	}
+
+	// Maintenant on peu accepter des connexions des clients
+	// /!\ LA FONCTION ACCEPT EST BLOQUANTE 
+
+	int socket_client ;
+	socket_client = accept ( socket_serveur , NULL , NULL );
+	if ( socket_client == -1)
+	{
+		perror ( " accept " );
+		/* traitement d ’ erreur */
+	}
+	/* On peut maintenant dialoguer avec le client */
+	const char * message_bienvenue = "Bonjour, bienvenue sur le serveur (ou pas)\n" ;
+	write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
+
+	return 0;
+}
