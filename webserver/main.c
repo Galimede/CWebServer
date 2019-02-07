acac4566cdd2d123a370e54efbd4d0df63d92899
Mathieu Degand <mat.degand@numericable.fr>
2019-02-07 09:25:00 +0100

    ajout initialisation signaux

--- webserver/main.c
+++ webserver/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
 #include "socket.h"
-int main(void) {
+
+void initialiser_signaux(void)
+{
+    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
+    {
+        perror("signal");
+    }
+}
+
+int main(void)
+{
     /* Arnold Robbins in yhe LJ of February '95, describing RCS */
     /*if (argc > 1 && strcmp(argv[1], "-advice") == 0) {
         printf("Don't Panic!\n");
@@ -9,8 +20,9 @@ int main(void) {
     }
     printf("Need an advice?\n");
     return 0;*/
-   creer_serveur(8080);      
+    
+    initialiser_signaux();
+    creer_serveur(8080);
 
     return 0;
-    
 }
