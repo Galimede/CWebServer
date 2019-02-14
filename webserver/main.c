93fad34dc78b0e41f35a9679f2c800a47de87eb9
Francois Lannoy <flannoy154@gmail.com>
2019-02-14 09:31:04 +0100

    on va commencer le serv web

--- webserver/main.c
+++ webserver/main.c
@@ -6,41 +6,55 @@
 #include <sys/socket.h>
 #include "socket.h"
 #include <sys/wait.h>
+
+void traitement_signal ( int sig )
+{
+  int status =0;
+  waitpid(-1,&status, WNOHANG);
+
+  printf ( " Signal %d reçu\n " , sig );
+}
 void initialiser_signaux(void)
 {
-    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
-    {
-        perror(" signal ");
-    }
+  struct sigaction sa ;
+  sa.sa_handler = traitement_signal ;
+  sigemptyset (&sa . sa_mask );
+  sa.sa_flags = SA_RESTART ;
+
+  if ( sigaction ( SIGCHLD , &sa , NULL ) == -1){
+    perror ( " sigaction ( SIGCHLD ) " );
+  }
+  if (signal(SIGPIPE, SIG_IGN) == SIG_ERR){
+    perror(" signal ");
+  }
 }
 
 int main(void)
 {
-    initialiser_signaux();
-    int s_serveur = creer_serveur(8080);
+  initialiser_signaux();
+  int s_serveur = creer_serveur(8080);
 
-    int socket_client;
-    while (1)
+  int socket_client;
+  while (1)
+  {
+    /* On attend la connexion du client */
+    socket_client = accept(s_serveur, NULL, NULL);
+    pid_t pid = fork();
+
+    if (socket_client == -1)
     {
-        /* On attend la connexion du client */
-        socket_client = accept(s_serveur, NULL, NULL);
-        pid_t pid = fork();
-
-        if (socket_client == -1)
-        {
-            perror(" accept ");
-            /* traitement d ’ erreur */
-        }
-        if (pid == 0){
-          /* On peut maintenant dialoguer avec le client */
-          const char *message_bienvenue = "Bonjour\nbienvenue sur le serveur (ou pas)\nc'est encore en construction\nmais ne vous inquietez pas \non travaille dur pour le rendre op sous peu \nj'espere que ca vous plaira\nen attendant allez prendre un cafe\nou un croissant qui sait ?\nOu...\nrevenez plus tard";
-          write(socket_client, message_bienvenue, strlen(message_bienvenue));
-        } else{
-          close(socket_client);
-          wait(NULL);
-        }
-
+      perror(" accept ");
+      /* traitement d ’ erreur */
+    }
+    if (pid == 0){
+      /* On peut maintenant dialoguer avec le client */
+      const char *message_bienvenue = "Bonjour\nbienvenue sur le serveur (ou pas)\nc'est encore en construction\nmais ne vous inquietez pas \non travaille dur pour le rendre op sous peu \nj'espere que ca vous plaira\nen attendant allez prendre un cafe\nou un croissant qui sait ?\nOu...\nrevenez plus tard";
+      write(socket_client, message_bienvenue, strlen(message_bienvenue));
+    } else{
+      close(socket_client);
     }
 
-    return 0;
+  }
+
+  return 0;
 }
