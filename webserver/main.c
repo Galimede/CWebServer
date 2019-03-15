7958677b6c057b5115cc3fb04300ac532d76816f
Galimede <mat.degand@numericable.fr>
2019-03-15 14:20:37 +0100

    indentation

--- webserver/main.c
+++ webserver/main.c
@@ -12,26 +12,26 @@
 
 void traitement_signal(int sig)
 {
-  int status = 0;
-  waitpid(-1, &status, WNOHANG);
+	int status = 0;
+	waitpid(-1, &status, WNOHANG);
 
-  printf(" Signal %d reçu\n ", sig);
+	printf(" Signal %d reçu\n ", sig);
 }
 void initialiser_signaux(void)
 {
-  struct sigaction sa;
-  sa.sa_handler = traitement_signal;
-  sigemptyset(&sa.sa_mask);
-  sa.sa_flags = SA_RESTART;
+	struct sigaction sa;
+	sa.sa_handler = traitement_signal;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
 
-  if (sigaction(SIGCHLD, &sa, NULL) == -1)
-  {
-    perror(" sigaction ( SIGCHLD ) ");
-  }
-  if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
-  {
-    perror(" signal ");
-  }
+	if (sigaction(SIGCHLD, &sa, NULL) == -1)
+	{
+		perror(" sigaction ( SIGCHLD ) ");
+	}
+	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
+	{
+		perror(" signal ");
+	}
 }
 
 int main(void)
@@ -65,7 +65,7 @@ int main(void)
 			http_request request;
 			int parse_request = parse_http_request(buffer, &request);
 			printf("TARGET ----> %s\n", request.target);
-			
+
 			if (parse_request == 0)
 				send_response(client, 400, "Bad Request", "Bad request\r\n");
 			else if (request.method == HTTP_UNSUPPORTED)
@@ -74,7 +74,7 @@ int main(void)
 				send_response(client, 200, " OK ", motd);
 			else
 				send_response(client, 404, "Not Found", "Not Found\r\n");
-			
+
 			fclose(client);
 		}
 		else
@@ -85,17 +85,17 @@ int main(void)
 	return 0;
 }
 
-  char * fgets_or_exit(char * buffer, int size, FILE * stream) 
-  {
-	  char * message = fgets(buffer, size, stream);
-	  if (message == NULL)
-		  exit(0);
-	  
-	  return message;
-  }
+char * fgets_or_exit(char * buffer, int size, FILE * stream)
+{
+	char * message = fgets(buffer, size, stream);
+	if (message == NULL)
+		exit(0);
 
-  void skip_headers(FILE * client)  {	  const int BUFFSIZE = 1024;	  char buffer[BUFFSIZE];	  do
-	  {
-		  fgets_or_exit(buffer, BUFFSIZE, client);
-	  } while (strcmp(buffer, "\r\n") != 0);  }  void send_status(FILE * client, int code, const char * reason_phrase)  {	  fprintf(client, "HTTP/1.1 %d %s\r\n", code, reason_phrase);  }  void send_response(FILE * client, int code, const char * reason_phrase, const char * message_body)   {	  send_status(client, code, reason_phrase);	  fprintf(client, "Host: localhost:8080\r\nConnection: close\r\nContent-Length: %li\r\n\r\n%s\r\n", strlen(message_body), message_body);  }
+	return message;
+}
+
+void skip_headers(FILE * client){	const int BUFFSIZE = 1024;	char buffer[BUFFSIZE];	do
+	{
+		fgets_or_exit(buffer, BUFFSIZE, client);
+	} while (strcmp(buffer, "\r\n") != 0);}void send_status(FILE * client, int code, const char * reason_phrase){	fprintf(client, "HTTP/1.1 %d %s\r\n", code, reason_phrase);}void send_response(FILE * client, int code, const char * reason_phrase, const char * message_body){	send_status(client, code, reason_phrase);	fprintf(client, "Host: localhost:8080\r\nConnection: close\r\nContent-Length: %li\r\n\r\n%s\r\n", strlen(message_body), message_body);}
 
