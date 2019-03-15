eb7a70c07dd8bf98032c3f12cc6e05041893fa83
Galimede <mat.degand@numericable.fr>
2019-03-15 14:18:36 +0100

    refactor clean

--- webserver/main.c
+++ webserver/main.c
@@ -7,6 +7,8 @@
 #include <sys/socket.h>
 #include "socket.h"
 #include <sys/wait.h>
+#include "prototype.h"
+#include "http_parse.h"
 
 void traitement_signal(int sig)
 {
@@ -34,68 +36,66 @@ void initialiser_signaux(void)
 
 int main(void)
 {
-  initialiser_signaux();
-  int s_serveur = creer_serveur(8080);
+	initialiser_signaux();
+	int s_serveur = creer_serveur(8080);
 
-  int socket_client;
-  while (1)
+	int socket_client;
+	while (1)
+	{
+		/* On attend la connexion du client */
+		socket_client = accept(s_serveur, NULL, NULL);
+		pid_t pid = fork();
+
+		if (socket_client == -1)
+		{
+			perror(" accept ");
+			/* traitement d ’ erreur */
+		}
+		if (pid == 0)
+		{
+			/* On peut maintenant dialoguer avec le client */
+			FILE *client = fdopen(socket_client, "w+");
+			const char *motd = "Bonjour\nbienvenue sur le serveur (ou pas)\nc'est encore en construction\nmais ne vous inquietez pas \non travaille dur pour le rendre op sous peu \nj'espere que ca vous plaira\nen attendant allez prendre un cafe\nou un croissant qui sait ?\nOu...\nrevenez plus tard";
+			const int MAXBUFFSIZE = 200;
+			char buffer[MAXBUFFSIZE];
+
+			fgets_or_exit(buffer, MAXBUFFSIZE, client);
+			printf("REQUEST --> %s", buffer);
+			skip_headers(client);
+			http_request request;
+			int parse_request = parse_http_request(buffer, &request);
+			printf("TARGET ----> %s\n", request.target);
+			
+			if (parse_request == 0)
+				send_response(client, 400, "Bad Request", "Bad request\r\n");
+			else if (request.method == HTTP_UNSUPPORTED)
+				send_response(client, 405, "Method Not Allowed", "Method Not Allowed\r\n");
+			else if (strcmp(request.target, "/") == 0)
+				send_response(client, 200, " OK ", motd);
+			else
+				send_response(client, 404, "Not Found", "Not Found\r\n");
+			
+			fclose(client);
+		}
+		else
+		{
+			close(socket_client);
+		}
+	}
+	return 0;
+}
+
+  char * fgets_or_exit(char * buffer, int size, FILE * stream) 
   {
-    /* On attend la connexion du client */
-    socket_client = accept(s_serveur, NULL, NULL);
-    pid_t pid = fork();
-
-    if (socket_client == -1)
-    {
-      perror(" accept ");
-      /* traitement d ’ erreur */
-    }
-    if (pid == 0)
-    {
-      /* On peut maintenant dialoguer avec le client */
-      FILE *fp = fdopen(socket_client, "w+");
-      const char *message_bienvenue = "Bonjour\nbienvenue sur le serveur (ou pas)\nc'est encore en construction\nmais ne vous inquietez pas \non travaille dur pour le rendre op sous peu \nj'espere que ca vous plaira\nen attendant allez prendre un cafe\nou un croissant qui sait ?\nOu...\nrevenez plus tard";
-      //fprintf(fp, "%s", message_bienvenue);
-      int taillemsg = 200;
-      char s[taillemsg];
-      //fgets(s, taillemsg, fp);
-      //strcat(s, "\r\n");
-      //printf("Client : %s\n",s);
-
-      /*char * retour = fgets(s, taillemsg, fp);
-      char *retourligne = strcat(s, "\r\n");
-      if (retourligne != NULL)
-      {
-        retourligne = '\0';
-      }*/
-      // On verifie si l'entête est correcte ou non
-      fgets(s, taillemsg, fp);
-      //strcat(s, "\r\n");
-      //s[strlen(s)-1] = '\0';
-      printf("Client : %s\n", s);
-      if (strncmp(s, "GET / HTTP/1.1", strlen("GET / HTTP/1.1")) == 0)
-      {
-        do
-        {
-          fgets(s, taillemsg, fp);
-        } while (strcmp(s, "\r\n") != 0);
-        fprintf(fp, "HTTP/1.1 200 OK\r\nHost: localhost:8080\r\nConnection: close\r\nContent-Length: %li\r\n\r\n%s\r\n", strlen(message_bienvenue), message_bienvenue);
-      }
-      else if (strcmp("GET / HTTP/1.1\r\n", s) != 0)
-      {
-        fprintf(fp, "HTTP/1.1 400 Bad Request\r\nHost: localhost:8080\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n");
-      }else if (strcmp("GET /inexistant HTTP/1.1\r\n", s) == 0)
-      {
-        fprintf(fp, "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-Length: 13\r\n\r\n404 Not Found\r\n");
-      }
-
-
-      fclose(fp);
-    }
-    else
-    {
-      close(socket_client);
-    }
+	  char * message = fgets(buffer, size, stream);
+	  if (message == NULL)
+		  exit(0);
+	  
+	  return message;
   }
 
-  return 0;
-}
+  void skip_headers(FILE * client)  {	  const int BUFFSIZE = 1024;	  char buffer[BUFFSIZE];	  do
+	  {
+		  fgets_or_exit(buffer, BUFFSIZE, client);
+	  } while (strcmp(buffer, "\r\n") != 0);  }  void send_status(FILE * client, int code, const char * reason_phrase)  {	  fprintf(client, "HTTP/1.1 %d %s\r\n", code, reason_phrase);  }  void send_response(FILE * client, int code, const char * reason_phrase, const char * message_body)   {	  send_status(client, code, reason_phrase);	  fprintf(client, "Host: localhost:8080\r\nConnection: close\r\nContent-Length: %li\r\n\r\n%s\r\n", strlen(message_body), message_body);  }
+
